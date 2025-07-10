d1329bd6d6290a0203c9da21fe1a9afaa42aac9f
Harshil Savaj <savajharshil99@gmail.com>
2025-07-10 23:10:52 +0530

    commit1
    Added basic frame to the project

---
+++ restaurant/routes.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, render_template, request, redirect
+import pyodbc
+import json
+
+main = Blueprint('main', __name__)
+
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    r'SERVER=harshilssqlserver.database.windows.net;'
+    'DATABASE=SampleDB;'
+    'UID=hsavaj;'
+    'PWD=pass@word1'
+)
+cursor = conn.cursor()
+
+@main.route('/')
+def show_menu():
+    cursor.execute("SELECT * FROM menuitems")
+    menu = cursor.fetchall()
+    return render_template('menu.html', menu=menu)
+
+@main.route('/place_order', methods=['POST'])
+def place_order():
+    mobile_number = request.form['mobile_number']
+    order_data = request.form['order_data']
+    items = json.loads(order_data)
+
+    for item_id, item in items.items():
+        name = item['name']
+        qty = item['qty']
+
+        cursor.execute(
+            "INSERT INTO Orders (mobile_number, item_id, name, qty) VALUES (?, ?, ?, ?)",
+            (mobile_number, item_id, name, qty)
+        )
+
+    conn.commit()
+    return redirect('/')
+
+
+
+@main.route('/kitchen')
+def kitchen():
+    cursor.execute("""
+        SELECT id, name, qty, mobile_number, order_time, status, priority
+        FROM Orders
+        WHERE status = 'pending'
+        ORDER BY priority DESC, order_time ASC
+    """)
+    orders = cursor.fetchall()
+    return render_template("kitchen.html", orders=orders)
+
+
+@main.route('/update_status/<int:order_id>', methods=['POST'])
+def update_status(order_id):
+    cursor.execute("UPDATE Orders SET status = 'ready' WHERE id = ?", (order_id,))
+    conn.commit()
+    return redirect('/kitchen')
