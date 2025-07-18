596019514495d3a68fbc5db96af08b59bee06bc1
Harshil Savaj <savajharshil99@gmail.com>
2025-07-19 00:08:50 +0530

    commit3

--- restaurant/__init__.py
+++ restaurant/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    #app.config.from_pyfile('../instance/config.py')
+    #app.config.from_pyfile('../instance/config.py') 
     
     #db.init_app(app)
 
