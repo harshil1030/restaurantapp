4912fdaff673042ecb2e4beb7b2c0be518cead1a
Harshil Savaj <savajharshil99@gmail.com>
2025-07-19 00:29:01 +0530

    Update __init__.py

--- restaurant/__init__.py
+++ restaurant/__init__.py
@@ -2,9 +2,6 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    #app.config.from_pyfile('../instance/config.py') 
-    
-    #db.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
